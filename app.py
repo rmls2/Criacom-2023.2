0f2fd73db834c713a94f52c5a35e517f68fec778
rmls2 <millerrobert182@gmail.com>
2024-03-13 09:19:21 -0300

    changes in app.py

--- app.py
+++ app.py
@@ -6,16 +6,23 @@ from api_keys import YOUR_API_KEY
 from gemini import generate_history, translate_history
 
 descricao_paragrafo = []
+qtd_imagens = 0
+lista_numeros_imagens = []
 
 def main():  
 
     # Using "with" notation
     with st.sidebar:
-        st.title("Chaves das API's")
-        clipdro_key = st.text_input('Chave da API Clipdrop', type="password")
+        st.header("Chaves das API's")
+        clipdrop_key = st.text_input('Chave da API Clipdrop', type="password")
         gemini_key = st.text_input('Chave da API GEMINI', type="password")
-        prompt_clip = clipdro_key
+        prompt_clip = clipdrop_key
         
+        st.header("Quantidade de imagens")
+
+        numero_imagens = st.slider("selecione a quantidade de imagens que deseja gerar para a história.", min_value=1, max_value=10, value=10)
+        qtd_imagens = numero_imagens
+
     st.title("Criacomp_AI")
     st.subheader("Dê asas a sua imaginação com o Criacomp_AI")
     st.write("O criador de histórias usando um único prompt de texto.")
@@ -28,7 +35,7 @@ def main():
 
     if submit_button:
 
-        generate_history(gemini_key)
+        generate_history(gemini_key, qtd_imagens)
         translate_history(gemini_key)
 
         count_paragrafo = 1        
@@ -51,6 +58,8 @@ def main():
                             f.write(r.content)
                         print("Imagem salva com sucesso!")
                         count_paragrafo+=1
+                        lista_numeros_imagens.append(count_paragrafo)
+
                     else:
                         r.raise_for_status()
                 
@@ -69,21 +78,20 @@ def exibir_historia():
     if st.toggle('exibir história'):
         descricao_paragrafos = ler_descricoes_de_arquivo("./historia_gemini.txt")
 
-        #col1, col2, col3 = st.columns(3)
-
-        #with col1:
-        st.subheader("Paragrafo 1")
-        st.image("./images/image_1.jpg")
-        st.write(descricao_paragrafos[0])
+        for i in range(1,3):
+            #with col1:
+            st.subheader(f"Paragrafo {i}")
+            st.image(f"./images/image_{i}.jpg")
+            st.write(descricao_paragrafos[i-1])
         #with col2:
-        st.subheader("Paragrafo 2")
-        st.image("./images/image_2.jpg")
-        st.write(descricao_paragrafos[1])
+        #st.subheader("Paragrafo 2")
+        #st.image("./images/image_2.jpg")
+        #st.write(descricao_paragrafos[1])
 
         #with col3:
-        st.subheader("Paragrafo 3")
-        st.image("./images/image_3.jpg")
-        st.write(descricao_paragrafos[2])
+        #st.subheader("Paragrafo 3")
+        #st.image("./images/image_3.jpg")
+        #st.write(descricao_paragrafos[2])
 
 if __name__ == "__main__":
     main()
