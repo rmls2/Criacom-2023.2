5cb7e7982d52e30b0f2fa6ba047376f6c6d502db
rmls2 <millerrobert182@gmail.com>
2024-03-12 23:14:58 -0300

    add  sidebar in app scripts and new api field.

--- app.py
+++ app.py
@@ -4,13 +4,22 @@ import requests
 import time 
 from api_keys import YOUR_API_KEY
 from gemini import generate_history, translate_history
-from funcoes_auxiliares import limpar_arquivos_de_texto, nome_arquivos
 
 descricao_paragrafo = []
 
-def main():
-    st.title("Criacom.AI")
-    st.write("criatividade e acessibilidade (shoraste?)!")
+def main():  
+
+    # Using "with" notation
+    with st.sidebar:
+        st.title("Chaves das API's")
+        clipdro_key = st.text_input('Chave da API Clipdrop', type="password")
+        gemini_key = st.text_input('Chave da API GEMINI', type="password")
+        prompt_clip = clipdro_key
+        
+    st.title("Criacomp_AI")
+    st.subheader("Dê asas a sua imaginação com o Criacomp_AI")
+    st.write("O criador de histórias usando um único prompt de texto.")
+
     # Caixa de texto
     _input = st.text_input("escreva a história que deseja gerar:")
     with open('inputusuario.txt', 'w') as f:
@@ -19,8 +28,8 @@ def main():
 
     if submit_button:
 
-        generate_history()
-        translate_history()
+        generate_history(gemini_key)
+        translate_history(gemini_key)
 
         count_paragrafo = 1        
         with open('./historia_traduzida.txt', 'r') as historia:
@@ -34,7 +43,7 @@ def main():
                     files = {
                     'prompt': (None, TEXT_PROMPT, 'text/plain')
                     },
-                    headers = { 'x-api-key': YOUR_API_KEY}
+                    headers = { 'x-api-key': prompt_clip}
                     )
 
                     if (r.ok):
@@ -58,23 +67,23 @@ def ler_descricoes_de_arquivo(nome_arquivo):
     
 def exibir_historia():
     if st.toggle('exibir história'):
-        descricao_paragrafos = ler_descricoes_de_arquivo("./historia_traduzida.txt")
+        descricao_paragrafos = ler_descricoes_de_arquivo("./historia_gemini.txt")
 
-        col1, col2, col3 = st.columns(3)
+        #col1, col2, col3 = st.columns(3)
 
-        with col1:
-            st.header("Paragrafo 1")
-            st.image("./images/image_1.jpg")
-            st.write(descricao_paragrafos[0])
-        with col2:
-            st.header("Paragrafo 2")
-            st.image("./images/image_2.jpg")
-            st.write(descricao_paragrafos[1])
+        #with col1:
+        st.subheader("Paragrafo 1")
+        st.image("./images/image_1.jpg")
+        st.write(descricao_paragrafos[0])
+        #with col2:
+        st.subheader("Paragrafo 2")
+        st.image("./images/image_2.jpg")
+        st.write(descricao_paragrafos[1])
 
-        with col3:
-            st.header("Paragrafo 3")
-            st.image("./images/image_3.jpg")
-            st.write(descricao_paragrafos[2])
+        #with col3:
+        st.subheader("Paragrafo 3")
+        st.image("./images/image_3.jpg")
+        st.write(descricao_paragrafos[2])
 
 if __name__ == "__main__":
     main()
