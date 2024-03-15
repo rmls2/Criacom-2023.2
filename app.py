8e78a4927def83b5aac401552b5527014d4a829b
rmls2 <millerrobert182@gmail.com>
2024-03-14 23:04:24 -0300

    update app.py

--- app.py
+++ app.py
@@ -95,21 +95,21 @@ def ler_descricoes_de_arquivo(nome_arquivo):
         st.error(f"Erro ao ler o arquivo: {e}")
         return None
 
-def create_voice_for_text_to_voice(x: list):
-    for i in x:
-        client = ElevenLabs(api_key="f5fd5ed78d745f4ab6ca714fc4546a79")
+# def create_voice_for_text_to_voice(x: list):
+#     for i in x:
+#         client = ElevenLabs(api_key="f5fd5ed78d745f4ab6ca714fc4546a79")
 
-        audio = client.generate(
-        text= x[i],
-        voice="Rachel",
-        model="eleven_multilingual_v2"
-        )
+#         audio = client.generate(
+#         text= x[i],
+#         voice="Rachel",
+#         model="eleven_multilingual_v2"
+#         )
 
-        # Converta o gerador em bytes concatenando-o
-        audio_bytes = b"".join(audio)
+#         # Converta o gerador em bytes concatenando-o
+#         audio_bytes = b"".join(audio)
 
-        #play(audio_bytes)
-        save(audio_bytes, './audios_elevenlabs/part_' + str(x.index(i)) + '.mp3')
+#         #play(audio_bytes)
+#         save(audio_bytes, './audios_elevenlabs/part_' + str(x.index(i)) + '.mp3')
 
 def list_audio_files(folder_path):
     audio_files = [file for file in os.listdir(folder_path) if file.endswith(".mp3") or file.endswith(".wav")]
